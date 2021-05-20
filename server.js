a311487cdbfdcbd1d13111c3d182c4e8ac545da7
eric-cruz <eacruz@live.com>
2021-05-20 09:30:39 -0500

    Changes made to server for deployment

--- server.js
+++ server.js
@@ -26,12 +26,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'client/dist')))
 app.use('/api/garden', garden);
 
+//Serve Static Assets in Production:
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
-
-//Routes:
-// app.get('/', (res, req) => {
-//   res.status(400)
-// })
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on Port ${PORT}`);
