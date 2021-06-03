815e8629155a20bd9bd42d66eb3d503d7052fdbe
eric-cruz <eacruz@live.com>
2021-06-03 13:31:37 -0500

    Final touches to MVP

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //DB Config/Connection:
-const db = require('./config/keys').mongoURI;
+const db = process.env.mongoURI
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
@@ -23,17 +23,17 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'client/dist')))
+app.use(express.static(path.join(__dirname, 'client/build')))
 app.use('/api/garden', garden);
 
 //Serve Static Assets in Production:
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+// if(process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
-}
+// }
 
 app.listen(PORT, () => {
   console.log(`Server listening on Port ${PORT}`);
