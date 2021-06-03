224bbc621b172b7c869229a0dbe2b84c816ac7b4
eric-cruz <eacruz@live.com>
2021-06-03 17:10:23 -0500

    Made changes to static asset serve

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use('/api/garden', garden);
 
 //Serve Static Assets in Production:
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  // app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'))
